ad562443023c3e7530beada28355398afd1b451f
amit2093 <er.amit1493@gmail.com>
2018-12-12 21:46:19 +0530

    minor

--- MyStory/src/main/java/com/MyStory/Comment/CommentController.java
+++ MyStory/src/main/java/com/MyStory/Comment/CommentController.java
@@ -3,12 +3,9 @@ package com.MyStory.Comment;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.MyStory.Person.PersonDto;
