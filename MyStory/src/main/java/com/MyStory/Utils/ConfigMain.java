9d2d20c2df98825d873f8f7c2a7a942bc4732679
amit2093 <er.amit1493@gmail.com>
2018-12-12 21:43:34 +0530

    minor

---
+++ MyStory/src/main/java/com/MyStory/Utils/ConfigMain.java
@@ -0,0 +1,17 @@
+package com.MyStory.Utils;
+
+public class ConfigMain {
+	
+	public static final String ANGULAR_URL = "http://localhost:4200";
+	
+	public static final String GET_ALL_FEEDS = "/getAllFeeds";
+	public static final String GET_FEED_VIA_FEED_ID = "/getFeed/{feedId}";
+	public static final String SAVE_FEEDS = "saveFeeds";
+	
+	public static final String GET_ALL_COMMENTS = "/getAllComments";
+	public static final String SAVE_COMMENT = "/saveComment";
+	
+	public static final String GET_ALL_PERSONS= "getAllPersons";
+	public static final String SAVE_PERSON = "/savePerson";
+	
+}
